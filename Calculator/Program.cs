51ddca5dfb60928a66383e02cb3a1e1f3fc8d8c3
Robert Ranta <robert.jalmari.ranta@tktk.ee>
2024-04-25 20:10:16 +0300

    add week7 code

---
+++ Calculator/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("Enter the operation (+/-):");
+char useroperator = char.Parse(Console.ReadLine());
+
+
+Console.WriteLine("Enter a:");
+int firstNum = Int32.Parse(Console.ReadLine());
+Console.WriteLine("Enter b:");
+int secondNum = Int32.Parse(Console.ReadLine());
+
+switch (useroperator)
+{
+    case '+':
+        Addition(firstNum, secondNum);
+        break;
+    case '-': 
+        Subraction(firstNum, secondNum);
+        break;
+    default:
+        Console.WriteLine("invalid operator");
+        break;
+}
+
+
+
+static void Addition(int a, int b)
+{
+    Console.WriteLine($"{a} + {b} = {a + b}");
+}
+
+static void Subraction(int a, int b)
+{
+    Console.WriteLine($"{a} - {b} = {a - b}");
+}
+
+
---
+++ Functions/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Are you coming or leaving? (in/out):");
+string userchoice = Console.ReadLine();
+
+if (userchoice == "in")
+{
+    PrintHello();  
+}
+else
+{
+    PrintGoodbye();
+}
+
+
+
+
+
+
+
+
+static void PrintHello() // function ehk meetod
+{
+    Console.WriteLine("Hello, world!");
+}
+
+static void PrintGoodbye()
+{
+    Console.WriteLine("See you later, alligator");
+}
